ec0f963ee5810dd30d12c2083eb45604c4e66ad8
miguel-henrique <miguelhenrique.as10@gmail.com>
2022-01-10 22:59:12 -0300

    update

--- pages/components/search.js
+++ pages/components/search.js
@@ -1,6 +1,8 @@
+
 import { useCallback, useRef, useState } from 'react'
 import Link from 'next/link'
-import { Input, NavbarText } from 'reactstrap'
+import { Input } from 'reactstrap'
+import axios from 'axios';
 
 export default function Search() {
 
@@ -10,6 +12,21 @@ export default function Search() {
   const [results, setResults] = useState([])
 
   const searchEndpoint = (query) => `https://newsapi.org/v2/everything?q=${query}&from=2022-01-07&sortBy=popularity&apiKey=c6595ed7516847f4a77b8fc01f2f9e6f`
+
+
+
+  /* 
+  const searchEndpoint = async () =>{
+    const response = await axios.get(
+        'https://newsapi.org/v2/everything?q=${query}&from=2022-01-07&sortBy=popularity&apiKey=c6595ed7516847f4a77b8fc01f2f9e6f'
+    );
+
+
+    return { dados: response.data}
+
+}
+ */
+
 
   const onChange = useCallback((event) => {
     const query = event.target.value;
@@ -38,32 +55,30 @@ export default function Search() {
   }, [])
 
   return (
+    
     <div
+      //className={styles.container}
       ref={searchRef}
     >
-
-      <NavbarText>
-          <div>
       <Input
+        //className={styles.search}
         onChange={onChange}
         onFocus={onFocus}
         placeholder='Search posts'
         type='text'
         value={query}
       />
-      </div>
-      </NavbarText>
-      {/* { active && results.length > 0 && (
-        <ul>
-          {results.map(({ title }) => (
-            <li>
-              <Link>
+      { active && results.length > 0 && (
+        <ul className={styles.results}>
+          {results.map(({ id, title }) => (
+            <li className={styles.result} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
             </li>
           ))}
         </ul>
-      ) } */}
+      ) }
     </div>
   )
 }
