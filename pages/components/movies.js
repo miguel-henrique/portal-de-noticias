28087edaacb1e04646f35d721d2f49179fc8625e
miguel-henrique <miguelhenrique.as10@gmail.com>
2022-07-28 12:00:55 -0300

    update

--- pages/components/movies.js
+++ pages/components/movies.js
@@ -4,7 +4,31 @@ import React from "react";
 import Movie from "./movie";
 
 
-function Movies ( props ) {
+/* function Movies ( props ) {
+
+
+  {console.log(props)};
+
+ const dataResult = Object.entries(props).map(([key, value]) => ({ [key]: value }));
+
+console.log(dataResult);
+
+  let cards = <h3>Loading...</h3>;
+
+
+  if (dataResult != null) {
+    cards = dataResult.map((m, i) => <Movie key={i} item={m} />);
+  }
+
+  return (
+    <Container >
+      <Card >{cards}</Card>
+    </Container>
+  );
+}; */
+
+
+const Movies = ( props ) => {
 
 
   {console.log(props)};
@@ -26,5 +50,7 @@ console.log(dataResult);
     </Container>
   );
 };
+
+
 
 export default Movies;
